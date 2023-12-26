a6afbee532479c0eb93b042fba900c673e35c52a
Sohil0070 <153104236+Sohil0070@users.noreply.github.com>
2023-12-26 10:33:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,9 +25,9 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://imagesupload.net/images/hAcYz.jpeg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/61ef9818986cef9554017.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/61ef9818986cef9554017.jpg")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/61ef9818986cef9554017.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://imagesupload.net/images/hAcYz.jpeg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://imagesupload.net/images/hAcYz.jpeg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://imagesupload.net/images/hk5WM.jpeg")
 
 # Admins, Channels & User
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
