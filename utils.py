069755969a2b522b2eea23c62f69e65fbae4c7ff
Sohil0070 <153104236+Sohil0070@users.noreply.github.com>
2023-12-28 10:11:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -537,7 +537,7 @@ async def get_verify_shorted_link(num, link):
         https = "https"
         link = link.replace("http", https)
 
-    if URL == "api.link2cash.in":
+    if URL == "api.urlspay.in":
         url = f"https://{URL}/shortLink"
         params = {"token": API,
                   "format": "json",
